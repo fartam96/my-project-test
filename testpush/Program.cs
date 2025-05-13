e489507442fc40aef37101659d047818835f2914
Farkas Tamás <fartam96@gmail.com>
2025-05-13 18:59:13 +0200

    add project

---
+++ testpush/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace testpush
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, test!");
+
+            var szam = 5;
+
+            var test = testMethod();
+
+            Console.WriteLine(szam == test);
+
+           
+        }
+
+        private static int testMethod()
+        {
+            return 5;
+        }
+    }
+}
